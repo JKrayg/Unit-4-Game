78bab63f4bfae7c6163bdeb7dc8b38e3f6905794
Jake Krayger <jkrayg@protonmail.com>
2020-03-03 13:02:55 -0500

    Adding variables and rng

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+var cgNumber = "";
+var userScore = 0;
+var wins = 0;
+var losses = 0;
+
+cgNumber = Math.floor(Math.random() * 120) + 9;
+$("#cgnumber").html(cgNumber);
+console.log(cgNumber);
+
+var crystal1 = Math.floor(Math.random() * 12) + 1;
+var crystal2 = Math.floor(Math.random() * 12) + 1;
+var crystal3 = Math.floor(Math.random() * 12) + 1;
+var crystal4 = Math.floor(Math.random() * 12) + 1;
+
+
+});
+
+
+  
