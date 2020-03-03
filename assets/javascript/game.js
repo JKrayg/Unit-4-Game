344f8e73908c896eb55b25926c06176ce369c196
Jake Krayger <jkrayg@protonmail.com>
2020-03-03 13:41:40 -0500

    adding reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
 
 var cgNumber = "";
@@ -14,7 +16,21 @@ var crystal2 = Math.floor(Math.random() * 12) + 1;
 var crystal3 = Math.floor(Math.random() * 12) + 1;
 var crystal4 = Math.floor(Math.random() * 12) + 1;
 
+$("#wins").text("Wins: " + wins);
+$("#losses").text("Losses: " + losses);
 
+function reset() {
+    cgNumber = Math.floor(Math.random() * 120) + 9;
+        $("#cgnumber").html(cgNumber);
+    userScore = 0;
+        $("#userscore").text(userScore);
+    crystal1 = Math.floor(Math.random() * 12) + 1;
+    crystal2 = Math.floor(Math.random() * 12) + 1;
+    crystal3 = Math.floor(Math.random() * 12) + 1;
+    crystal4 = Math.floor(Math.random() * 12) + 1;
+}
+
+    
 });
 
 
