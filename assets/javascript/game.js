d48d08eb193aafe3a94c5dc1f576f2cd5f1f0553
Jake Krayger <jkrayg@protonmail.com>
2020-03-03 21:27:37 -0500

    Adding on.click function for each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,107 @@
-
-
 $(document).ready(function() {
 
-var cgNumber = "";
-var userScore = 0;
-var wins = 0;
-var losses = 0;
-
-cgNumber = Math.floor(Math.random() * 120) + 9;
-$("#cgnumber").html(cgNumber);
-console.log(cgNumber);
-
-var crystal1 = Math.floor(Math.random() * 12) + 1;
-var crystal2 = Math.floor(Math.random() * 12) + 1;
-var crystal3 = Math.floor(Math.random() * 12) + 1;
-var crystal4 = Math.floor(Math.random() * 12) + 1;
-
-$("#wins").text("Wins: " + wins);
-$("#losses").text("Losses: " + losses);
-
-function reset() {
-    cgNumber = Math.floor(Math.random() * 120) + 9;
-        $("#cgnumber").html(cgNumber);
-    userScore = 0;
-        $("#userscore").text(userScore);
-    crystal1 = Math.floor(Math.random() * 12) + 1;
-    crystal2 = Math.floor(Math.random() * 12) + 1;
-    crystal3 = Math.floor(Math.random() * 12) + 1;
-    crystal4 = Math.floor(Math.random() * 12) + 1;
-}
-
+    var cgNumber = "";
+    var wins = 0;
+    var losses = 0;
+    var userScore = 0;
     
-});
+    
+    $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + losses)
+    $("#cgnumber").html(cgNumber);
+    
+    $("#userscore").text("Your Score: " + userScore);
+    cgNumber = Math.floor(Math.random() * 120) + 19;
+    $("#cgnumber").html(cgNumber);
+    
+    var crystal1 = Math.floor(Math.random() * 12) + 1;
+    console.log(crystal1);
+    var crystal2 = Math.floor(Math.random() * 12) + 1;
+    console.log(crystal2);
+    var crystal3 = Math.floor(Math.random() * 12) + 1;
+    console.log(crystal3);
+    var crystal4 = Math.floor(Math.random() * 12) + 1;
+    console.log(crystal4);
+    
+    function reset() {
+        userScore = 0;
+        $("#userscore").text("Your Score: " + userScore);
+        cgNumber = Math.floor(Math.random() * 120) + 19;
+        $("#cgnumber").html(cgNumber);
+        crystal1 = Math.floor(Math.random() * 12) + 1;
+        console.log(crystal1);
+        crystal2 = Math.floor(Math.random() * 12) + 1;
+        console.log(crystal2);
+        crystal3 = Math.floor(Math.random() * 12) + 1;
+        console.log(crystal3);
+        crystal4 = Math.floor(Math.random() * 12) + 1;
+        console.log(crystal4);
+    
+    }
+    
+    
+    
+    $("#crystal-1").on("click", function() {
+        userScore = userScore + crystal1;
+        $("#userscore").text("Your Score: " + userScore);
+        if (userScore === cgNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            reset();
+        } else if (userScore > cgNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses)
+            reset();
+        }
+    
+    });
+    
+    $("#crystal-2").on("click", function() {
+        userScore = userScore + crystal2;
+        $("#userscore").text("Your Score: " + userScore);
+        if (userScore === cgNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            reset();
+        } else if (userScore > cgNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses)
+            reset();
+        }
+    
+    });
+    
+    $("#crystal-3").on("click", function() {
+        userScore = userScore + crystal3;
+        $("#userscore").text("Your Score: " + userScore);
+        if (userScore === cgNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            reset();
+        } else if (userScore > cgNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses)
+            reset();
+        }
+    
+    });
+    
+    $("#crystal-4").on("click", function() {
+        userScore = userScore + crystal4;
+        $("#userscore").text("Your Score: " + userScore);
+        if (userScore === cgNumber) {
+            wins++;
+            $("#wins").text("Wins: " + wins);
+            reset();
+        } else if (userScore > cgNumber) {
+            losses++;
+            $("#losses").text("Losses: " + losses)
+            reset();
+        }
+    
+    });
+    
+    });
 
 
   
